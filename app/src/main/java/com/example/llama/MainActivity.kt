2b21b346656b114c5e94ac938375da664df90fc3
Dhruv_Anand <ananddhruv475@gmail.com>
2024-04-22 18:12:40 +0530

    added automatic loading of model when download is 100%

--- app/src/main/java/com/example/llama/MainActivity.kt
+++ app/src/main/java/com/example/llama/MainActivity.kt
@@ -261,7 +261,7 @@ fun MainCompose(
                             )
                             if(!viewModel.getIsSending()){
 
-                                IconButton(onClick = { viewModel.send() }) {
+                                IconButton(onClick = { viewModel.send()  }) {
                                     Icon(
                                         imageVector = Icons.Default.Send,
                                         contentDescription = "Send",
