e0cd96bb949b4732d578e9beb00ca20afd5a1bcf
Nikhil Tyagi <34955087+Kid-Kisuke@users.noreply.github.com>
2024-05-08 13:12:54 +0530

    Devnik context update (#30)
    * Chore: Testing context
    
    * Chore: Testing context
    
    * chore: changed user icon
    
    * chore: changed user icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.llama"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
